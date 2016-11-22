29cbbdc95fe246883038a745523ca5dc22f6c2e6
SOAS <SOAS@DESKTOP-VDCPOTR>
2016-11-22 19:48:58 +0900

    MemberController

--- src/main/java/org/juke/web/MemberController.java
+++ src/main/java/org/juke/web/MemberController.java
@@ -1,22 +1,42 @@
 package org.juke.web;
 
+import javax.inject.Inject;
+
+import org.juke.domain.MemberVO;
+import org.juke.service.MemberServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin
 @Controller
 @RequestMapping("/member/*")
 public class MemberController {
-	
-	private static final Logger logger =
-			LoggerFactory.getLogger(MemberController.class);
-	
-	@GetMapping
-	public String singup(){
-		return null;
-	}
 
+   private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 
+   @PostMapping("/signup")
+   public String signup(@RequestBody MemberVO vo) {
+
+      logger.info("vo" + vo);
+
+      return "success";
+   }
+
+   @GetMapping("/login")
+   public void getLogin() {
+
+   }
+
+   @PostMapping("/login")
+   public void registPOST(MemberVO vo, Model model) throws Exception{
+      
+      
+     }
 }
