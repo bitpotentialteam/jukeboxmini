3f85e2ed565c46a175acec2ee20ab97207d4b5d7
SOAS <SOAS@DESKTOP-VDCPOTR>
2016-11-22 11:03:36 +0900

    commit

--- src/test/java/org/juke/web/MemberTester.java
+++ src/test/java/org/juke/web/MemberTester.java
@@ -1,12 +1,16 @@
 package org.juke.web;
 
-import javax.inject.Inject;
 
 import org.juke.dao.MemberDAOImpl;
 import org.juke.domain.MemberVO;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
 public class MemberTester {
 	
 	@Autowired
