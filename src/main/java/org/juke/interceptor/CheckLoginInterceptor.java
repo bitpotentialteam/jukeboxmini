835caf6443ff5ac496fd9818073e5e1ba473c843
SOAS <SOAS@DESKTOP-VDCPOTR>
2016-11-23 10:24:01 +0900

    LoginInterceptor, CheckLoginInterceptor

---
+++ src/main/java/org/juke/interceptor/CheckLoginInterceptor.java
@@ -0,0 +1,30 @@
+package org.juke.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.juke.web.MemberController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class CheckLoginInterceptor extends HandlerInterceptorAdapter {
+
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute("LOGIN") != null) {
+			return true;
+		}
+
+		response.sendRedirect("login");
+		return false;
+	}
+
+}
