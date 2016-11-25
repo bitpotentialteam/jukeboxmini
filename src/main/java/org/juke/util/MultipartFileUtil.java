c4e9e6e62467f11411c7377a7ab1119811df0401
SOAS <SOAS@DESKTOP-VDCPOTR>
2016-11-25 10:19:12 +0900

    현장 테스트로 직행....

---
+++ src/main/java/org/juke/util/MultipartFileUtil.java
@@ -0,0 +1,18 @@
+package org.juke.util;
+
+public class MultipartFileUtil {
+
+	
+	MultipartFileUtil file;
+	
+
+	public MultipartFileUtil getFile() {
+		
+		return file;
+	}
+
+	public void setFile(MultipartFileUtil file) {
+		this.file = file;
+	}
+
+}
